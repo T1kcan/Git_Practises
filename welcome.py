a478edc9e157d2dfec9c950dc2698ddc24980379
D1680_Tamer <tbincan@gmail.com>
2022-11-26 18:11:37 +0300

    added new features

--- welcome.py
+++ welcome.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 def hello():
     return "<h1>WELCOME TO TURAN CYBER HUB</h1>"  \
            "<h2>Git Class Hands On</h2>"
+@app.route('/home')
+def home():
+    return "<h1>Welcome home<h1>" \
+           "<h2>Git Class Hands On</h2>"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
 
