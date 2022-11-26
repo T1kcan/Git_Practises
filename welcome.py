5e0f8b36fa55836c62c0bd980bf9d2df6853bc53
D1680_Tamer <tbincan@gmail.com>
2022-11-26 15:27:53 +0300

    added third line one more time for the sake of mixed mode

--- welcome.py
+++ welcome.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "<h1>WELCOME TO TURAN CYBER HUB</h1>"  \
-           "<h2>Git Class Hands On</h2>"
+           "<h2>Git Class Hands On</h2>" \
+           "<h3>Advanced Moves<h3>"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
 
